97c2c2d8dd6b89f7ebe4da23ed7810cb5b4943e0
Giacomo Calabresi <42006598+G0D0T@users.noreply.github.com>
2020-02-04 16:58:54 +0100

    Add files via upload

---
+++ dpVerifier.py
@@ -0,0 +1,62 @@
+def find_part(s):
+    n = len(s)
+    k = sum(s)
+    if k % 2:
+        return False
+    k2 = k // 2
+    rows = k2 + 1
+    cols = n + 1
+    p = [[True for i in range(cols)] for j in range(rows)]
+    for i in range(1, rows):
+        p[i][0] = False
+    for i in range(1, rows):
+        for j in range(1, cols):
+            p[i][j] = p[i][j - 1]
+            if i >= s[j - 1]:
+                p[i][j] = (p[i][j] or p[i - s[j - 1]][j - 1])
+
+    return p[k2][n]
+
+
+def main():
+    # s = [8, 6, 3, 2, 1, 2, 1, 1]
+    # s = [44, 50, 1, 82, 75, 83, 2, 30, 20, 8]
+    # s = [21, 97, 68, 38, 13, 30, 87, 70, 6, 43]
+    # s = [42, 99, 2, 94, 54, 50, 76, 53, 55, 42, 35, 44, 58, 98, 76, 80, 51, 41, 95, 23] #ott
+    # s = [82, 24, 85, 94, 1, 89, 99, 75, 50, 8, 39, 33, 47, 8, 35, 49, 66, 96, 98, 1, 7, 72, 36, 53, 1, 13, 78, 2, 42, 91, 63, 57, 74, 99, 64, 22, 12, 80, 84, 50, 60, 83, 64, 65, 35, 74, 1, 73, 98, 26]
+    # s = [75, 50, 2, 75, 74, 99, 38, 8, 8, 98, 47, 38, 8, 15, 6, 13, 82, 98, 54, 79, 6, 38, 27, 12, 33, 21, 4, 50, 28, 68, 37, 88, 43, 68, 64, 76, 69, 26, 57, 81, 16, 45, 1, 31, 82, 89, 83, 27, 79, 83]
+    # s = [98, 36, 21, 63, 77, 1, 25, 89, 88, 47, 10, 11, 77, 14, 17, 64, 44, 1, 55, 86] #ott
+    # s = [33, 80, 1, 60, 25, 95, 19, 10, 66, 29, 21, 23, 58, 53, 55, 13, 27, 89, 99, 83] #not
+    # print(s)
+    filename = 'solutions.txt'
+    with open(filename) as f:
+        content = f.readlines()
+    content = [x.strip() for x in content]
+
+    lista = []
+    out = []
+    for i, c in enumerate(content):
+        elem = []
+        lista.append(c.split('], '))
+        first = lista[i][0][2:]
+        temp = [int(n) for n in first.split(', ')]
+        elem.append(temp)
+        second = lista[i][1][1:]
+        temp = [int(n) for n in second.split(', ')]
+        elem.append(temp)
+        third = lista[i][2][:-1]
+        elem.append(int(third))
+        out.append(elem)
+
+    for s in range(len(out)):
+        if find_part(out[s][0]):
+            print('Sottoinsiemi ottimi.')
+        else:
+            print('Non ottimi...')
+        if out[s][2]:
+            print('Non ottimi GA.\n')
+        else:
+            print('Sottoinsiemi ottimi GA!\n')
+
+
+main()
---
+++ ga.py
@@ -0,0 +1,136 @@
+import random
+
+
+# Funzione che calcola il "costo" della partizione, dato il cromosoma e la lista dei valori
+def partition(x, y):
+    a = b = 0
+    for i in range(len(x)):
+        # Se nel cromosoma ho 0 all'i-esimo bit, metto nella partizione a l'i-esimo elemento
+        if not x[i]:
+            a += y[i]
+        # Viceversa, con un bit a 1 metto nella partizione b l'i-esimo elemento
+        else:
+            b += y[i]
+    # Ritorno il valore assoluto (più è vicino a 0 e migliore è la partizione)
+    return abs(a - b)
+
+
+def gen_population(s, n):
+    population = []
+    for i in range(0, s):
+        indv = []
+        # Ogni cromosoma contiene n bit (contenenti 0 o 1), pari alla dimensione della lista di interi
+        for j in range(n):
+            indv.append(random.randint(0, 1))
+        population.append(indv)
+    return population
+
+
+# Funzione che esegue il crossover a un punto, dati i due genitori e il punto in questione
+def crossover(g1, g2, p):
+    f1 = g1[:p] + g2[p:]
+    f2 = g1[p:] + g2[:p]
+    # Ritorna i due figli della coppia
+    return f1, f2
+
+
+# Funzione per gestire la fase di mutazione
+def mutate(g1, dim):
+    gene = random.randrange(0, dim)
+    # In base al numero random generato, inverto il bit a quella posizione nel cromosoma
+    if g1[gene]:
+        g1[gene] = 0
+    else:
+        g1[gene] = 1
+    # Ritorno il cromosoma modificato
+    return g1
+
+
+def main(problem, numbers, real_size, xover, mutation, generations):
+    # Controllo che la grandezza della popolazione sia pari (per avere tutte coppie)
+    sum_numbers = sum(numbers)
+    size = real_size + real_size % 2
+    population = gen_population(size, len(numbers))
+    
+    # Ciclo fino al numero di generazioni dato in input
+    for i in range(1, generations + 1):
+        print('Generazione: ', i)
+        j = 0
+        # Lista che raccoglie i risultati della funzione costo
+        scores = []
+        # Ciclo per ogni coppia della popolazione
+        while j <= size - 2:            
+            # print(population[j])
+            # print(population[j+1])
+            # Genero un numero casuale che determina se la coppia avrà figli (probabilità alta)
+            mating = random.random()
+            if mating > xover:
+                # Genero un secondo numero random per il punto del crossover
+                point = random.randrange(0, size)
+                figlio1, figlio2 = crossover(population[j], population[j+1], point)
+
+                # Nuovamente genero un numero per eventuali mutazioni di uno o più figli (probabilità bassa)
+                evolution1 = random.random()
+                evolution2 = random.random()
+                if evolution1 > mutation:
+                    figlio1 = mutate(figlio1, len(numbers))
+                if evolution2 > mutation:
+                    figlio2 = mutate(figlio2, len(numbers))
+
+                # Assegno alle seguenti variabili il valore della funzione costo
+                oldscore1 = problem(population[j], numbers)
+                newscore1 = problem(figlio1, numbers)
+                oldscore2 = problem(population[j+1], numbers)
+                newscore2 = problem(figlio2, numbers)
+
+                # Controllo chi ha il valore minore (genitore o figlio) e lo inserisco in lista per la prossima generazione
+                if newscore1 < oldscore1:
+                    population[j] = figlio1
+                    scores.append(newscore1)
+                else:
+                    scores.append(oldscore1)
+                if newscore2 < oldscore2:
+                    population[j+1] = figlio2
+                    scores.append(newscore2)
+                else:
+                    scores.append(oldscore2)
+            # Se i cromosomi non hanno avuto figli, passano direttamente loro alla prossima generazione
+            else:
+                scores.append(problem(population[j], numbers))
+                scores.append(problem(population[j+1], numbers))
+            j += 2        
+            
+        print(scores)
+        # Calcolo varie statistiche e stampo su terminale
+        gen_avg = sum(scores) / size
+        gen_best = min(scores)
+        gen_sol = population[scores.index(min(scores))]
+        print('> GENERATION AVERAGE:', gen_avg)
+        print('> GENERATION BEST:', gen_best)
+        print('> BEST SOLUTION:', gen_sol, '\n')
+        
+        # controllo se ho trovato la soluzione ottima: smetto di far evolvere la popolazione
+        if gen_best == sum_numbers % 2:
+            break
+
+    return gen_sol, gen_best
+
+
+# Parametri per l'algoritmo genetico GA
+problem = partition
+dimensione = 25
+# y = [1, 2, 3, 4, 4, 3, 2, 1, 1, 1]
+y = []
+for i in range(dimensione):
+    y.append(random.randrange(1, 100))
+size = 10
+xover = 0.1
+mutation = 0.9
+generazioni = 20
+
+soluzione, valore = main(problem, y, size, xover, mutation, generazioni)
+print(y)
+sol = (y, soluzione, valore)
+# Stampo su file per controllo
+with open('solutions.txt', 'a') as f:
+    f.write(str(sol)+'\n')
